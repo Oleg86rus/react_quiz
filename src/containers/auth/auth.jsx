89519a704dea10804582ff6057e8b33b93a3f4be
oleg086rus <o.pototskiy@yandex.ru>
2022-05-01 21:49:15 +0500

    some change

--- src/containers/auth/auth.jsx
+++ src/containers/auth/auth.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import './auth.css'
+import Button from '../../components/UI/button/button'
 
 const Auth = () => {
+  const loginHandler = () => {
+  
+  }
+  const registerHandler = () => {
+  
+  }
+  const submitHandler = (event) => {
+    event.preventDefault()
+  }
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className='Auth'>
+      <div>
+        <h1>Авторизация</h1>
+        <form onSubmit={submitHandler} className='AuthForm'>
+          <input type="text"/>
+          <input type="text"/>
+          <Button
+            type='success'
+            onClick={loginHandler}
+          >
+            Войти
+          </Button>
+          <Button
+            type='primary'
+            onClick={registerHandler}
+          >
+            Зарегестрироваться
+          </Button>
+        </form>
+      </div>
     </div>
   )
 }
